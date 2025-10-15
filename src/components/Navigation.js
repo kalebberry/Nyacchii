66eeecbb4d5021f82d8fe6b4603f137269e6f653
Kaleb Berry <kaleb.berry@outlook.com>
2025-10-14 21:47:07 -0500

    updating

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -26,14 +26,12 @@ const Navigation = () => {
 		}
 	}
 	return (
-        <section className='navigation'>
+		<section className='navigation'>
 			<div className='navigation-container'>
 				<div className={`${Nav.Logo}`}>
 					<Link href='/'>
-
-                        <Image src={Logo} />
-
-                    </Link>
+						<Image src={Logo} />
+					</Link>
 				</div>
 
 				<button
@@ -51,61 +49,33 @@ const Navigation = () => {
 						ref={navRef}
 						className={`${Nav.primary_navigation}`}>
 						<li>
-<<<<<<< HEAD
-							<Link href='/' className={router.pathname == '/' ? `${Nav.active}` : ''}>
-=======
 							<Link className={router.pathname == '/' ? `${Nav.active}` : ''} href='/'>
->>>>>>> 8068b67a33ffb7db44c0470a32ae88f4e2a63eb4
 								Home
 							</Link>
 						</li>
 						<li>
 							<Link
-<<<<<<< HEAD
-                                href='/commission'
-                                className={router.pathname == '/commission' ? `${Nav.active}` : ''}>
-								
-									Commission
-								
-=======
 								className={router.pathname == '/commission' ? `${Nav.active}` : ''}
 								href='/commission'>
 								Commission
->>>>>>> 8068b67a33ffb7db44c0470a32ae88f4e2a63eb4
 							</Link>
 						</li>
 						<li>
 							<Link
-<<<<<<< HEAD
-                                href='/portfolio'
-                                className={router.pathname == '/portfolio' ? `${Nav.active}` : ''}>
-=======
 								className={router.pathname == '/portfolio' ? `${Nav.active}` : ''}
 								href='/portfolio'>
->>>>>>> 8068b67a33ffb7db44c0470a32ae88f4e2a63eb4
 								Portfolio
 							</Link>
 						</li>
 						<li>
-<<<<<<< HEAD
-							<Link
-                                href='/shop'
-                                className={router.pathname == '/shop' ? `${Nav.active}` : ''}>
-=======
 							<Link className={router.pathname == '/shop' ? `${Nav.active}` : ''} href='/shop'>
->>>>>>> 8068b67a33ffb7db44c0470a32ae88f4e2a63eb4
 								Shop
 							</Link>
 						</li>
 						<li>
 							<Link
-<<<<<<< HEAD
-                                href='/contact'
-                                className={router.pathname == '/contact' ? `${Nav.active}` : ''}>
-=======
 								className={router.pathname == '/contact' ? `${Nav.active}` : ''}
 								href='/contact'>
->>>>>>> 8068b67a33ffb7db44c0470a32ae88f4e2a63eb4
 								Contact
 							</Link>
 						</li>
@@ -113,7 +83,7 @@ const Navigation = () => {
 				</nav>
 			</div>
 		</section>
-    );
+	);
 };
 
 export default Navigation;
