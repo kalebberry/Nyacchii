f8f31f4fc8bbb62ded9fd23db6c3f82a40f76704
Kaleb Berry <kaleb.berry@outlook.com>
2025-10-14 23:05:19 -0500

    updating

--- src/hydrate/landing.ts
+++
@@ -1,5 +0,0 @@
-import { initPinia } from '@/utils/initPinia';
-import InteractiveCounter from '@/components/vue/landing/InteractiveCounter.vue';
-
-// Mount the InteractiveCounter component to the '#interactive-counter' element
-initPinia(InteractiveCounter, '#interactive-counter');
